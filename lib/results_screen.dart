7e35d1f1383010ce7360f93b1c6763b500f584e2
Bhavikgadher <gadherbhavik@gmail.com>
2024-11-15 12:42:10 +0530

    update textStyled and listing the question answers

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,25 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary/questions_summary.dart';
+
+import 'CustomWidget/styled_text.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({super.key, required this.chosenAnswers, required this.onRestart});
+
+  final List<String> chosenAnswers;
+  final void Function() onRestart;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].questions,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i]
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
-            const Text('You ansewer x out of y'),
+            StyledText(
+                'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+                28,
+                colors: Colors.white70,
+                align: TextAlign.center),
             const SizedBox(height: 30),
-            const Text('xyz'),
+            QuestionsSummary(summaryData),
             const SizedBox(height: 30),
-            TextButton(
-              child: const Text("Restart Quiz!"),
-              onPressed: () {},
+            TextButton.icon(
+              icon: const Icon(Icons.refresh),
+              label: const StyledText("Restart", 14,
+                  colors: Colors.white70, align: TextAlign.center),
+              onPressed: onRestart,
             )
           ],
         ),
